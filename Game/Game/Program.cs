9de422d2e83b632fbd6db8496704ac16b6d53d2c
exect3r <exect3r@gmail.com>
2017-07-30 09:15:47 +0100

    A modified Main method. Required for GUI to work.

---
+++ Game/Game/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Game {
+    public static class Program {
+
+        // This is used to access important fields like Content and GraphicsDevice.
+        static internal Game Game;
+
+        [STAThread]
+        static void Main() {
+            Game = new Game();
+            Game.Run();
+        }
+    }
+}
