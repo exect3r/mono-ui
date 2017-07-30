52e6c344c06e7f39bfa6f8ca30301a51470cc8a3
exect3r <exect3r@gmail.com>
2017-07-30 12:07:34 +0100

    Basic GUI element's interface

---
+++ Game/Game/GUI/IElement.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.GUI {
+    interface IElement {
+        void Update(GameTime gameTime);
+        void Draw(SpriteBatch spriteBatch, GameTime gameTime);
+        IElement Delete();
+        bool IsMarkedForDelete();
+        IElement SetVisible(bool visible);
+        bool IsVisible();
+        string GetId();
+        IElement SetX(float x);
+        float GetX();
+        IElement SetY(float y);
+        float GetY();
+        IElement SetTexture(Texture2D texture);
+        Texture2D GetTexture();
+        IElement SetParent(IElement parent);
+        IElement GetParent();
+    }
+}
