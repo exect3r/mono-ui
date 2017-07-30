264218e81a365eb0b73554ca89ad5d58a03e252c
exect3r <exect3r@gmail.com>
2017-07-30 12:01:20 +0100

    Custom events implementations.

---
+++ Game/Game/EventHandlers.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.GUI {
+    class MouseEventArgs {
+        int X, Y;
+        ButtonState LeftButton, RightButton, MiddleButton;
+        internal MouseEventArgs(int x, int y, 
+            ButtonState lb = ButtonState.Released, ButtonState rb = ButtonState.Released, ButtonState mb = ButtonState.Released) {
+
+            X = x; Y = y;
+            LeftButton = lb; RightButton = rb; MiddleButton = mb;
+        }
+    }
+
+    delegate bool MouseEventHandler(object sender, MouseEventArgs args);
+}
